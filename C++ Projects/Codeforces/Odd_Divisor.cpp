13bf2679b8f47626f6c5af543fd5e992f8815d3b
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-04-26 00:09:00 +0530

    25/04/23

---
+++ C++ Projects/Codeforces/Odd_Divisor.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        long long a;
+        cin >> a;
+
+        if (a % 2 == 0)
+        {
+            cout << "NO" << endl;
+        }
+
+        else
+            cout << "YES" << endl;
+    }
+}
