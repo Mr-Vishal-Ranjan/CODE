6fbd3b6c32bc30dd3fd0a7e37bb03082db5c06d6
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-04-29 11:50:09 +0530

    v

---
+++ C++ Projects/OOPS/test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str, a;
+
+    cin >> str;
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        a[i] = str[str.size() - i - 1];
+    }
+
+    cout << a;
+}
