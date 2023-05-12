18ded17ecb5f7cdc86181ce0536d3193a203f23b
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-05-12 17:43:44 +0530

    OOPS

---
+++ C++ Projects/OOPS/Inheritance.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class A
+{
+
+private:
+    int a;
+
+protected:
+    void setValue(int k)
+    {
+        a = k;
+    }
+};
+
+class B : public A
+{
+
+public:
+    void setData(int x)
+    {
+        setValue(x);
+    }
+};
+
+int main()
+{
+    B obj;
+    obj.setData(4);
+}
