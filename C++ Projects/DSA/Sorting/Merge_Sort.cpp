de5c9219b63056ee74f8b70a29518c1a760f17e0
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-05-13 11:09:14 +0530

    DSA

---
+++ C++ Projects/DSA/Sorting/Merge_Sort.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[3] = {8, 9, 10};
+    int arr1[3] = {1, 2, 3};
+    int final[6];
+
+    int i = 0;
+    int j = 0, k = 0;
+
+    while (i < 3 && j < 3)
+    {
+        if (arr[i] > arr1[j])
+        {
+            final[k] = arr1[j];
+            j++;
+            k++;
+        }
+
+        else
+        {
+            final[k] = arr[i];
+            i++, k++;
+        }
+    }
+
+    while (i < 3)
+    {
+        final[k] = arr[i];
+        i++, k++;
+    }
+
+    while (j < 3)
+    {
+        final[k] = arr1[j];
+        j++;
+        k++;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << final[i];
+    }
+    
+}
