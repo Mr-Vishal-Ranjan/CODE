6acfd49ef0e5b6b447ad648e1da7b27d76940808
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-06-18 00:34:25 +0530

    18/06/23

---
+++ JAVA Projects/Contructor/Student5.java
@@ -0,0 +1,25 @@
+package Contructor;
+class Student5{
+    int id;
+    String name;
+    int age;
+    Student5(int i,String n){
+        id = i;
+        name = n;
+    }
+    Student5(int i,String n,int a){
+        id = i;
+        name = n;
+        age=a;
+    }
+    void display(){System.out.println(id+" "+name+" "+age);}
+
+    public static void main(String args[]){
+        //Student5 s3 = new Student5(); error
+        Student5 s1 = new Student5(111,"Karan");
+        Student5 s2 = new Student5(222,"Aryan",25);
+        s1.display();
+        s2.display();
+        //s3.display();
+    }
+}
