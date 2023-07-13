07f5a4b815c4c385d9012fb75df53450c4abbc33
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-07-13 19:17:21 +0530

    13/07/23

---
+++ JAVA Projects/Watermelon/wt.java
@@ -0,0 +1,14 @@
+package Watermelon;
+import java.util.*;
+public class wt {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a%2==0){
+            System.out.print("YES");
+        }else{
+            System.out.print("NO");
+        }
+    }
+}
