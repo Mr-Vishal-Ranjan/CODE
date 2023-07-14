594e7f2ed1084cc353358ec3b2513d2205488576
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-07-14 22:27:03 +0530

    14/07/23

---
+++ JAVA Projects/Doubly_LL/Reverse.java
@@ -0,0 +1,23 @@
+package Doubly_LL;
+
+public class Reverse {
+    public static D_LL.Node reverse(D_LL.Node head) {
+        D_LL.Node temp = head;
+        D_LL.Node n;
+        D_LL.Node p;
+        while (temp != null){
+            n = temp.next;
+            p = temp.prev;
+            temp.prev = n;
+            temp.next = p;
+            head = temp;
+            temp = temp.prev;
+        }
+        return head;
+    }
+    public static void main(String[] args) {
+        D_LL.Node head = D_LL.create_ll();
+        head = reverse(head);
+        D_LL.print_ll(head);
+    }
+}
