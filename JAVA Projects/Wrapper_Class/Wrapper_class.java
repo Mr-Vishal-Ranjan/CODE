ed2d8657868a531379115d2443a7a7e2fa4053bf
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-09 20:55:15 +0530

    09/08/23

--- JAVA Projects/Wrapper_Class/Wrapper_class.java
+++ JAVA Projects/Wrapper_Class/Wrapper_class.java
@@ -4,7 +4,7 @@ public class Wrapper_class {
         Integer i1 = Integer.valueOf("007");
         Double d1 = Double.valueOf("3.14");
         int a = Integer.parseInt("007");
-        double d = Double.parseDouble("3.14");
+        double d = Double.parseDouble("3.144");
         int c = i1.intValue();
 
         System.out.println(c);
