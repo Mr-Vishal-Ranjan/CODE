12c285d4ecf125d0086d333d1cce5477986fdb9d
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-09 20:53:30 +0530

    09/08/23

---
+++ JAVA Projects/Wrapper_Class/Wrapper_class.java
@@ -0,0 +1,12 @@
+package Wrapper_Class;
+public class Wrapper_class {
+    public static void main(String[] args) {
+        Integer i1 = Integer.valueOf("007");
+        Double d1 = Double.valueOf("3.14");
+        int a = Integer.parseInt("007");
+        double d = Double.parseDouble("3.14");
+        int c = i1.intValue();
+
+        System.out.println(c);
+    }
+}
