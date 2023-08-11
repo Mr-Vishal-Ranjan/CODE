d83823fc118e01b8c6e5901a61c0544671107776
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-11 13:00:16 +0530

    11/08/23

--- JAVA Projects/Access_Modifiers/Test.java
+++ JAVA Projects/Access_Modifiers/Test.java
@@ -1,7 +1,10 @@
 package Access_Modifiers;
 public class Test {
-    Outer_Class obj = new Outer_Class();
-    Outer_Class.Inner1 obj1 = new Outer_Class.Inner1();
-
+    //Outer_Class obj = new Outer_Class();
+    //Outer_Class.Inner1 obj1 = new Outer_Class.Inner1();
+    public static void main(String[] args) {
+        Outer_Class.Inner1.setX(7);
+        System.out.println(Outer_Class.Inner1.getX());
+    }
     //Outer_Class_Inner_Class.Inner2 obj2 = new Outer_Class_Inner_Class.Inner2();
 }
