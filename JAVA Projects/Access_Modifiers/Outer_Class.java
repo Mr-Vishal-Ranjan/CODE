1d75df6b2611d4b723cc81487ec5f56f2b87edbc
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-11 12:40:22 +0530

    11/08/23

--- JAVA Projects/Access_Modifiers/Outer_Class.java
+++ JAVA Projects/Access_Modifiers/Outer_Class.java
@@ -1,22 +1,28 @@
 package Access_Modifiers;
 public class Outer_Class {
     private static class Inner{
-        public static int x;
+        public int x;
+        public static void setX(int y){
+            y = x;
+        }
+        public static int getX(){
+            return x;
+        }
     }
     public static class Inner1{
-        public static int x;
+        public int x;
     }
     protected static class Inner2{
-        public static int x;
-    }
-        public static void Testing_Class(){
-            Inner.x = 1;
-            Inner1.x = 11;
-            Inner2.x = 22;
-    }
+        public int x;
 
+    }
+    public void set() {
+        Inner.setX(1);
+        //Inner1.x = 11;
+        //Inner2.x = 22;
+    }
     public void Show(){
-        System.out.println("Show ex "+Inner1.x+Inner2.x+Inner.x);
+        System.out.println("Show ex "+Inner.getX());
     }
     Inner obj = new Inner();
     Inner1 obj0 = new Inner1();
@@ -26,6 +32,7 @@ class Test0 {
     public static void main(String[] args) {
         Outer_Class obj0 = new Outer_Class();
         System.out.println("Yo");
+        obj0.set();
         obj0.Show();
     }
 }
