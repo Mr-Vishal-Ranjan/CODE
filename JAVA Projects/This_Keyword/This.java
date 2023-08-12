94c74bfe20a68335a098fe147351f3015310ea71
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-12 21:04:34 +0530

    11/08/23

---
+++ JAVA Projects/This_Keyword/This.java
@@ -0,0 +1,15 @@
+package This_Keyword;
+
+public class This {
+    public int l , b , h ;
+
+    public void setL(int l){
+        this.l = l;
+    }
+    public void setB(int b){
+        this.b = b;
+    }
+    public void setH(int h){
+        this.h = h;
+    }
+}
