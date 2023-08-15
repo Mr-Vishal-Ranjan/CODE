8c63588569c3b5b91a6e562b86073fa430ca853e
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-15 22:50:06 +0530

    15/08/23

---
+++ JAVA Projects/Array/Array_2D.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Array_2D {
+    public static void main(String[] args) {
+        int arr[][] = new int[4][];
+        arr[0] = new int[2];
+        arr[1] = new int[4];
+
+        System.out.println("Length: "+arr[1].length);
+    }
+}
