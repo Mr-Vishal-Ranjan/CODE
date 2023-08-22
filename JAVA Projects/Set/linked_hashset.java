adfb2b44c9b65fbe937e7e212f77e74468952210
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-22 12:49:05 +0530

    22/08/23

---
+++ JAVA Projects/Set/linked_hashset.java
@@ -0,0 +1,18 @@
+package Set;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+public class linked_hashset {
+    public static void main(String[] args) {
+        LinkedHashSet l = new LinkedHashSet();
+        LinkedHashSet a = new LinkedHashSet(20);
+        LinkedHashSet b = new LinkedHashSet(10,(float)0.5f);
+
+        LinkedList x= new LinkedList();
+        x.add("One");
+        x.add("two"+"three");
+
+        LinkedHashSet c = new LinkedHashSet(x);
+    }
+}
