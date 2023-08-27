2a3667b61f7fb94e72a6dba859931d77298277f3
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-27 23:44:39 +0530

    27/08/23

---
+++ JAVA Projects/Searching/Linear_Searching.java
@@ -0,0 +1,10 @@
+package Searching;
+
+public class Linear_Searching {
+    public int search(int target,int arr[]){
+        for(int i = 0;i < arr.length ;i++){
+
+        }
+        return 0;
+    }
+}
