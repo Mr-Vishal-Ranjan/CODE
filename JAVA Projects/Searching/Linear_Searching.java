0b122616c1e0ff69c54dbefc68990b1bd3bc2731
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-27 23:56:18 +0530

    27/08/23

--- JAVA Projects/Searching/Linear_Searching.java
+++ JAVA Projects/Searching/Linear_Searching.java
@@ -1,10 +1,22 @@
 package Searching;
 
 public class Linear_Searching {
-    public int search(int target,int arr[]){
+    public static int search(int target,int arr[]){
         for(int i = 0;i < arr.length ;i++){
-
+            if (target == arr[i]){
+                return i;
+            }
         }
-        return 0;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int index = search(4,arr);
+        if (index != -1){
+            System.out.println("Target index is "+index);
+        }else{
+            System.out.println("Target not found");
+        }
     }
 }
