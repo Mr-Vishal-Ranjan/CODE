d57d6f12d77d2deb8154d6a99266563bb41e3d1c
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-28 10:39:34 +0530

    28/08/23

---
+++ JAVA Projects/Searching/Binary_Searchhing.java
@@ -0,0 +1,28 @@
+package Searching;
+
+public class Binary_Searchhing {
+    public static int search(int start,int end,int[] arr,int target){
+        if (start > end || end < start){
+            return -1;
+        }
+        int mid = start+(end-start)/2;
+        if(arr[mid] == target){
+            return mid;
+        }else if(target < arr[mid]){
+            end = mid - 1;
+        } else if (target > arr[mid]) {
+            start = mid + 1;
+        }
+        return search(start,end,arr,target);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int start = 0;int end = arr.length-1;
+        int index = search(start,end,arr,7);
+        if (index > -1)
+            System.out.println("target found on index "+index);
+        else
+            System.out.println("target not found");
+    }
+}
