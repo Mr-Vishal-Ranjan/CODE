2627eb0aa48b2cc773ca3524bf86b9704fa8cf11
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-08-29 22:57:13 +0530

    29/08/23

---
+++ JAVA Projects/Sorting/Bubble_Sort.java
@@ -0,0 +1,20 @@
+package Sorting;
+
+public class Bubble_Sort {
+    public static void sort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]) {
+                    arr[i] = arr[i] + arr[j];
+                    arr[j] = arr[i] - arr[j];
+                    arr[i] = arr[i] - arr[j];
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr={5,3,1,2,4};
+        sort(arr);
+    }
+}
