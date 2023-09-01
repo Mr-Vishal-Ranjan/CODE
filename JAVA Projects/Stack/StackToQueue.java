daafa301939d5236626ddb041b7a462118194c19
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-09-01 23:23:48 +0530

    01/09/23

--- JAVA Projects/Stack/StackToQueue.java
+++ JAVA Projects/Stack/StackToQueue.java
@@ -6,12 +6,10 @@ public class StackToQueue {
     public static void main(String[] args) {
         StacK s = new StacK();
         Queue q = new Queue();
-
         s.push(1);
         s.push(2);
         s.push(3);
         s.push(4);
-
         while(s.peek()>0){
             q.enqueue(s.peek());
             s.pop();
