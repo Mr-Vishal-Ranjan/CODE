29b8256512ab4690b8b4c1f0b1bd12bcfc6bc585
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-09-17 15:24:59 +0530

    17/09/23

---
+++ JAVA Projects/LeetCode/Perfect_Square.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class Perfect_Square {
+    public static boolean isPerfectSquare(int num) {
+        int start,mid,end;
+        start=1;end=num/2+1;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(mid==num/mid && mid*mid ==num){
+                return true;
+            }else if(mid>num/mid){
+                end = mid-1;
+            }else if(mid<num/mid){
+                start = mid+1;
+            }else break;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        isPerfectSquare(num);
+    }
+}
