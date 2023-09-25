b68cfcc1690825de6b3886c91c7193e908c8588d
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-09-26 00:03:30 +0530

    23/09/23

---
+++ JAVA Projects/LeetCode/Sum0.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class Sum0 {
+    public static long Sum(long n,long sum){
+        if(n>0){
+            sum+=n;
+            n--;
+            sum= Sum(n,sum);
+        }
+        return sum;
+    }
+    public static long sumFirstN(long n) {
+        // Write your code here.
+        long sum=0;
+        return Sum(n,sum);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumFirstN(50));
+    }
+}
