73d34280f2a26ce3212efd427d920e997a063fe3
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-09-25 23:23:12 +0530

    23/09/23

--- JAVA Projects/LeetCode/F_L.java
+++ JAVA Projects/LeetCode/F_L.java
@@ -29,7 +29,6 @@ public class F_L {
 //        }
         return list;
     }
-
     public static void main(String[] args) {
         int[] arr={1,2,5,6,3,5,7,8,4};
         System.out.println(Check(arr,5));
