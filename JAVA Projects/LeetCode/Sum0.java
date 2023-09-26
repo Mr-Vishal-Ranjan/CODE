79f4c528d5e08765b28cec9b242d97b392270036
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-09-26 21:31:03 +0530

    23/09/23

--- JAVA Projects/LeetCode/Sum0.java
+++ JAVA Projects/LeetCode/Sum0.java
@@ -1,18 +1,19 @@
 package LeetCode;
 
 public class Sum0 {
-    public static long Sum(long n,long sum){
-        if(n>0){
-            sum+=n;
+    public static long Sum(long n, long sum) {
+        if (n > 0) {
+            sum += n;
             n--;
-            sum= Sum(n,sum);
+            sum = Sum(n, sum);
         }
         return sum;
     }
+
     public static long sumFirstN(long n) {
         // Write your code here.
-        long sum=0;
-        return Sum(n,sum);
+        long sum = 0;
+        return Sum(n, sum);
     }
 
     public static void main(String[] args) {
