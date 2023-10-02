2122ccb48897b25e230dc41d1caffe2399464c9d
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-10-02 15:54:36 +0530

    28/09/23

--- JAVA Projects/LeetCode/Frequency.java
+++ JAVA Projects/LeetCode/Frequency.java
@@ -4,14 +4,37 @@ import java.util.HashMap;
 
 public class Frequency {
     public static int[] getFrequencies(int []v) {
-        int[] arr = new int[2];
-        HashMap<Integer, Integer> hash = new HashMap<>();
+        int[] arr=new int[2];
+        HashMap<Integer,Integer> hash=new HashMap<>();
 
-        for (int i = 0; i < v.length; i++) {
+        arr[0]=v[0];
+        arr[1]=v[0];
+
+        for(int i=0;i<v.length;i++){
             if(hash.get(v[i])!=null)
                 hash.put(v[i],hash.get(v[i])+1);
             else
                 hash.put(v[i],1);
+        }
+
+        for(int i=0;i<v.length;i++){
+            if(hash.get(arr[1])>=hash.get(v[i])){
+                if(hash.get(arr[1])==hash.get(v[i])){
+                    if(arr[1]>v[i]){
+                        arr[1]=v[i];
+                    }
+                }else{
+                    arr[1]=v[i];
+                }
+            }
+
+            if(hash.get(arr[0])<=hash.get(v[i])){
+                if(hash.get(arr[0])==hash.get(v[i])){
+                    if(arr[0]>v[i]){
+                        arr[0]=v[i];
+                    }
+                }else arr[0]=v[i];
+            }
         }
 
         return arr;
