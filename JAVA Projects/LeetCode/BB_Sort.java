b628be10fbdd7d25a4d74f0fd3c0e597f7b8a69a
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-10-05 17:21:22 +0530

    05/10/23

--- JAVA Projects/LeetCode/BB_Sort.java
+++ JAVA Projects/LeetCode/BB_Sort.java
@@ -4,7 +4,7 @@ public class BB_Sort {
     public static void rec(int[] arr,int n,int temp,int count){
 
         if(temp==n-1){
-            temp=0;count=1;
+            temp=0;count=1;n-=1;
         }
 
         if(arr[temp]>arr[temp+1]){
@@ -14,7 +14,7 @@ public class BB_Sort {
         }else count++;
         temp++;
 
-        if(count!=n){
+        if(count<n){
             rec(arr, n, temp,count);
         }
     }
@@ -24,7 +24,7 @@ public class BB_Sort {
     }
 
     public static void main(String[] args) {
-        int[] arr={5,3,4,1,2};
+        int[] arr={5,3,4,10,2};
         bubbleSort(arr,5);
     }
 }
