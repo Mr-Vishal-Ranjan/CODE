5cf0e68872d5bc0e6ca93ec0816186302aa95c25
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-10-28 00:00:10 +0530

    27/10/23

--- JAVA Projects/LeetCode/LongSubArray.java
+++ JAVA Projects/LeetCode/LongSubArray.java
@@ -1,32 +1,30 @@
 package LeetCode;
 
+import java.util.HashMap;
+
 public class LongSubArray {
-    public static int longestSubarrayWithSumK(int[] nums, long k) {
-        // Write your code here
-        int i = 0, j = 0, maxLen = 0;
-        long sum = 0;
-        while (j < nums.length) {
-            if(sum<k){
-                sum+=nums[j];
-                j++;
+    public static int getLongestSubarray(int []nums, int k) {
+        HashMap <Long,Integer> hash=new HashMap<>();
+        long sum=0;
+        int maxLen = 0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum==k){
+                maxLen=i+1;
             }
-            if (sum == k) {
-                if (maxLen < j - i + 1) {
-                    maxLen = j - i + 1;
-                }
-                j++;
+            if(!hash.containsKey(sum)){
+                hash.put(sum, i);
             }
-            if(sum>k){
-                sum-=nums[i];
-                i++;
+            if( hash.containsKey(sum-k)){
+                maxLen=Math.max(maxLen,i - hash.get(sum-k));
             }
         }
         return maxLen;
     }
 
     public static void main(String[] args) {
-        int[] a = {-1 ,0 ,1 ,1 ,-1 ,-1 ,0};
-        System.out.println(longestSubarrayWithSumK(a, 0));
+        int[] a = {1,1,1};
+        System.out.println(getLongestSubarray(a, 4));
     }
 }
 //      int maxLen=0;
