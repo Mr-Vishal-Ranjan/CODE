089d29e0d5ebb16a757cefacf8afb093711adb89
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-10-28 23:28:21 +0530

    28/10/23

---
+++ JAVA Projects/LeetCode/lowerBound.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+public class lowerBound {
+    public static int lowerBound(int []arr, int n, int x) {
+        // Write your code here
+        int start=0,end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid] < x){
+                start = mid;
+            }
+            if(arr[mid] > x){
+                end = mid;
+            }
+            if(arr[mid]==x){
+                return arr[mid-1];
+            }
+        }
+
+        if(x<arr[0]){
+            return 0;
+        }
+        return arr.length-1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,2,3};
+        lowerBound(arr,4,4);
+    }
+}
