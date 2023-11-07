aba7f1bec59ce5683961e6e98e381c47a1dbc5b4
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-11-07 19:54:58 +0530

    7/11/23

--- JAVA Projects/LeetCode/NumberOfOccurrence.java
+++ JAVA Projects/LeetCode/NumberOfOccurrence.java
@@ -2,33 +2,43 @@ package LeetCode;
 
 public class NumberOfOccurrence {
     public static int count(int[] arr, int n, int x) {
-        int l=0,r=n-1;
+        int low =-1, high = -1, l=0,r=n-1;
         while(l<=r){
             int mid=(l+r)/2;
 
             if(arr[mid]==x){
-                if(arr[l]!=x){
-                    l++;
-                }
-                if(arr[r]!=x){
-                    r--;
-                }
+                low = mid;
+                r=mid-1;
             }
 
-            if(arr[r]==x && arr[l]==x){
-                return r-l+1;
-            }
-
-            if(arr[mid]<x){
+            else if(x < arr[mid]){
+                r=mid-1;
+            }else{
                 l=mid+1;
             }
-            if(arr[mid]>x) r=mid-1;
         }
-        return 0;
+        l=0;r=n-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+
+            if(arr[mid]==x){
+                high = mid;
+                l=mid+1;
+            }
+
+            else if(x < arr[mid]){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        System.out.print(low +" " + high);
+
+        return high - low <= 0 ? 0 : high - low +1;
     }
 
     public static void main(String[] args) {
-        int[] arr={1 ,1 ,1 ,2 ,2 ,3 ,3};
-        System.out.print(count(arr,7,3));
+        int[] arr={ 4 ,10 ,10, 10, 10, 10, 10, 11, 12, 14, 14, 17, 19, 19 };
+        System.out.print(count(arr,14,10));
     }
 }
